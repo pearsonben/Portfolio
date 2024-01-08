8c36bcaed11d39b44a5c26677b0f947e486d4b4a
pears <pearsonben98@gmail.com>
2024-01-08 22:26:36 +0000

    Updated About page with an image of me and the dog.

---
+++ src/images/images.tsx
@@ -0,0 +1,6 @@
+
+const IMAGES = {
+    bog : new URL('../../public/images/BOG.jpg', import.meta.url).href
+}
+
+export default IMAGES;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import IMAGES from "../images/images";
 
 function About() {
   return (
@@ -10,15 +11,19 @@ function About() {
             I'm a Chester-based software developer with full-stack experience. I
             mostly work with ASP.NET Core on the backend, TypeScript/React on
             the frontend and Azure/Github for CI/CD. I really enjoy learning
-            stuff, and at some point when I find some free time (😅) I would
-            love to add <Link to="https://elixir-lang.org/">elixir</Link>{" "}
-            to my skillset, as well as learn more about microservices.
+            stuff, and at some point when I find a bit of free time (😅) I would
+            love to add <Link to="https://elixir-lang.org/">elixir</Link> to my
+            skillset, as well as learn more about microservices.
           </p>
           <p>
             When not working, I spend most of my time reading, rock climbing,
             and playing the occasional indie game. I also have a working breed
             dog who I spend a lot of time training and walking.
           </p>
+          <figure className="image-figure">
+            <img width="800px" src={IMAGES.bog} alt="reggie-in-field"></img>
+            <figcaption>Me teasing the dog.</figcaption>
+          </figure>
         </article>
       </section>
     </>
