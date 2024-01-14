d5a9d6b9bb8b8aff68ecafa09322df54f66e4c48
pears <pearsonben98@gmail.com>
2024-01-14 21:10:12 +0000

    Updated SGS&Co experience.

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -6,9 +6,7 @@ const experiences = [
     companyName: "DataScope Systems",
     link: "https://www.datascopesystems.com/",
     timeline: "09/2023 - Current",
-    bullets: [
-
-    ],
+    bullets: [],
     description: `Working on various full-stack .NET Core/TypeScript/React applications, ensuring accessibilty and performance requirements are met. Furthermore, I am working with a group of 3 to dockerise the application suite to optimise the developer workflow.`,
     tools: [
       Tool.CSharp,
@@ -25,7 +23,7 @@ const experiences = [
     companyName: "SGS&CO",
     link: "https://www.sgsco.com/",
     timeline: "04/2022 - 09/2023",
-    description: `In this role I was part of the automation team, where I was responsible for building the entire SDLC of a full-stack application to aggregate release notes, configuration management and metrics tracking for 3 line of business apps. I optimised and migrated a multitude of legacy stored procedures into EF Core which resulted in a 10x reduction in execution across the board, additionally I maintained and updated a legacy .NET Framework/JQuery application.`,
+    description: `In this role I was part of the automation team, where I was responsible for building the entire SDLC of a full-stack application to aggregate release notes, configuration management and metrics tracking for 3 line of business apps. I optimised and migrated a multitude of legacy stored procedures into EF Core which resulted in a 10x reduction in execution across the board. Designed and implemented a multi-stage deployment pipeline in Azure for 2 internal applications. Additionally, I maintained a legacy .NET Framework/JQuery application.`,
     tools: [
       Tool.CSharp,
       Tool.Dotnet,
