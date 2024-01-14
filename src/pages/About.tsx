d3a503e8e3f70949c9ab221e3dccead9927ae31e
pears <pearsonben98@gmail.com>
2024-01-14 22:05:32 +0000

    Sorted public vite assets.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -31,7 +31,7 @@ const About = () => {
           <figure className="image-figure">
             <img
               width="600px"
-              src="https://private-user-images.githubusercontent.com/47435728/296617153-16d6cfb8-2e9f-44b4-96f7-9c736b38aff5.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDUyNjcyOTQsIm5iZiI6MTcwNTI2Njk5NCwicGF0aCI6Ii80NzQzNTcyOC8yOTY2MTcxNTMtMTZkNmNmYjgtMmU5Zi00NGI0LTk2ZjctOWM3MzZiMzhhZmY1LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAxMTQlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMTE0VDIxMTYzNFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTMwMWI4ZWI3NDlmNjA2YWE0MjE4ZWQzYmZiZTg3ZjEwYzM3NGM5NGRhNzg0NWQxMjQ4MmQ4ZTJiZGQ5Nzk4ZDgmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.LPEzABDtVyr9UoAv0W9HbG8eOLZQaqjOiCBUZeqEw_0"
+              src="/BOG.jpg"
               alt="reggie-in-field"
             ></img>
             <figcaption>(bonus dog picture)</figcaption>
